f353e6a3e00576685dc46aa27c82b7123608f7ee
Gloria Ding <gloria.hc.ding@gmail.com>
2018-01-21 15:39:20 -0800

    home page animation

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -1,5 +1,23 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $('.exe')
+// welcoms/index.html.erb ----------------------------------------------
+// A mouse movement within the div with class home will trigger the animation
+
+   $(".home").one("mouseover", function(){
+     $(".display-2").css('position','relative');
+      $(".display-2").animate({left: '30px'}, "slow");
+      $(".display-2").animate({left: '0px'}, "slow");
+      $(".display-2").animate({left: '30px'}, "slow");
+      $(".display-2").animate({left: '0px'}, "slow", function(){
+          $('.display-4').eq(0).fadeIn(3000, function(){
+            $(this).fadeOut(3000, function(){
+              $('.display-4').eq(1).fadeIn(3000);
+            });
+          });
+      });
+    });
+
+
+
 });
