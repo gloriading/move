678a2aca1ff10fbd8ff7813e34c6f9d6019b812f
Gloria Ding <gloria.hc.ding@gmail.com>
2018-02-07 20:27:34 -0800

    add download image button

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,15 +1,14 @@
 const API_KEY = '8a5f3002b35c1962612527561da6bc7d0a9fe5adce14aba8a4250dd5b7d51f54'
 const Charting = {
-  get (params) {
+  get (dates) {
     return fetch(
-      `http://localhost:3000/api/v1/charts?start_date=${params}`,
+      `http://localhost:3000/api/v1/charts?start_date=${dates}`,
       {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': API_KEY
-        },
-        body: JSON.stringify(params)
+        }
       }
     )
     .then(res => {
