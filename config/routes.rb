83d57fa0ac98cd34d83ed2eafa690406baed80e9
Gloria Ding <gloria.hc.ding@gmail.com>
2018-02-12 00:24:08 -0800

    colour auto-complete for nested fields as well

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 namespace :api, defaults: { format: :json } do
   namespace :v1 do
-    resources :charts
+    resources :charts, only: [:index]
     resources :records
   end
 end
