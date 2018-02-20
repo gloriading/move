85341342a551ee9bd96e3a6740ab1558d132899a
Gloria Ding <gloria.hc.ding@gmail.com>
2018-02-20 11:51:21 -0800

    updated image format on image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -38,11 +38,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
 
+  # version :thumb do
+  #   process resize_to_fill: [50, 50]
+  # end
+  
+  # version :content do
+  #   process resize_to_limit: [300, 300]
+  # end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
