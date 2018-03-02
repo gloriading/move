7fb39db0a1e231014dc5d5a3206ae9c0837b341c
Gloria Ding <gloria.hc.ding@gmail.com>
2018-03-01 23:02:41 -0800

    refactored index action and added flash message for screens controller

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -1,29 +1,43 @@
 class ScreensController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
+    basic_setup
+    @screenshot = Gastly.screenshot(@target)
+    @screenshot.selector = '.simple-calendar'
+    @image = @screenshot.capture
+
+    if @image.save("public/images/#{@img_path}")
+      flash[:success] = 'Screenshot has been created successfully.'
+      create_screenshot_record_for_users
+      upload_screenshot_to_aws
+      redirect_to user_path(current_user)
+    end
+  end
+
+  private
+
+  def basic_setup
     id = current_user.id
     first_name = current_user.first_name
     base_url = Rails.env.production? ? "https://healthymove.herokuapp.com" : "http://localhost:3000"
-    target = "#{base_url}/users/#{id}?start_date=#{params[:start_date]}"
-    @screenshot = Gastly.screenshot(target)
-    @screenshot.selector = '.simple-calendar'
-    @image = @screenshot.capture
-    date_mark = params[:start_date].split('-')[0..1].join('-')
+    @target = "#{base_url}/users/#{id}?start_date=#{params[:start_date]}"
+    @date_mark = params[:start_date].split('-')[0..1].join('-')
+    @img_path = "image-#{id}-#{first_name}-#{@date_mark}.png"
+  end
 
-    # if @image.save("app/assets/images/image-#{id}-#{first_name}.png")
-    if @image.save("public/images/image-#{id}-#{first_name}-#{date_mark}.png")
-      Screenshot.create(
-        user: current_user,
-        path: "image-#{id}-#{first_name}-#{date_mark}.png",
-        display: date_mark
-      )
-      uploader = ScreenshotUploader.new
-      file = File.open("public/images/image-#{id}-#{first_name}-#{date_mark}.png")
-      uploader.store!(file)
-      redirect_to user_path(current_user)
-    end
+  def create_screenshot_record_for_users
+    Screenshot.create(
+      user: current_user,
+      path: @img_path,
+      display: @date_mark
+    )
+  end
 
+  def upload_screenshot_to_aws
+    uploader = ScreenshotUploader.new
+    file = File.open("public/images/#{@img_path}")
+    uploader.store!(file)
   end
 
 end
