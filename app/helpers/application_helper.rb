72989a72a29d771bc1d61840caf247950273d732
Gloria Ding <gloria.hc.ding@gmail.com>
2018-03-04 19:42:42 -0800

    added page title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def title(text)
+    content_for :title, text
+  end
+
   def asset_exists?(asset_name)
     Rails.application.assets.find_asset(asset_name).present?
   end
